098db54905f2ff4a6d24ef594d11b9e170f11f1f
jrbeverly <jonathan@jrbeverly.me>
2021-06-12 09:58:17 -0500

    deps(golang): Upgrade to latest

--- provision/14-golang.bash
+++ provision/14-golang.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
-curl -sSL -O --noproxy "*" https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
-tar -xvf go1.13.5.linux-amd64.tar.gz 
+version="1.16.5"
+curl -sSL -O --noproxy "*" https://dl.google.com/go/go${version}.linux-amd64.tar.gz
+tar -xvf go${version}.linux-amd64.tar.gz 
 mv go /usr/local 
-rm go1.13.5.linux-amd64.tar.gz
+rm go${version}.linux-amd64.tar.gz
